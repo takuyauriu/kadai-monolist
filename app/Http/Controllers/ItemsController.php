e0a7e0e29880f99d1abb52d564e3532aa0eb5722
EC2 Default User <ec2-user@ip-172-31-44-155.us-east-2.compute.internal>
2018-05-27 08:32:26 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -46,7 +46,13 @@ class ItemsController extends Controller
      */
     public function show($id)
     {
-        //
+        $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
 
     /**
